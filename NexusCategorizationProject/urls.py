2831e65919f8c7e3632f77f85a8ed263e7458a59
Peter Dawson <PeterDawson80@hotmail.com>
2019-12-22 12:56:24 +0000

    started views

--- NexusCategorizationProject/urls.py
+++ NexusCategorizationProject/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import text_query.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',text_query.views.contact, name='contact'),
 ]
