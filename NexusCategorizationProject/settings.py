bd47c3783cbb346e58599abe92d950d115df28d7
Peter Dawson <PeterDawson80@hotmail.com>
2019-12-22 20:56:11 +0000

    static done

--- NexusCategorizationProject/settings.py
+++ NexusCategorizationProject/settings.py
@@ -119,8 +119,11 @@ USE_L10N = True
 
 USE_TZ = True
 
+STATIFILES_DIRS = [
+    os.path.join(BASE_DIR, 'NexusCategorizationProject/static/')
+]
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
